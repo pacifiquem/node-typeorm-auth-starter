a83fe0a434c4e94da5ba84c12b134d7dd87fc1de
MURANGWA Pacifique <pacifiquemurangwa001@gmail.com>
2024-06-15 23:18:17 +0200

    feat: authentication middleware added

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+
+const authMiddleware = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+
+  if (!authHeader) {
+    return res.status(401).json({ message: "Authorization header is missing" });
+  }
+
+  const token = authHeader.split(" ")[1]; // Bearer <token>
+
+  if (!token) {
+    return res.status(401).json({ message: "Token is missing" });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({ message: "Invalid token" });
+    }
+
+    req.user = user;
+    next();
+  });
+};
+
+module.exports = authMiddleware;
